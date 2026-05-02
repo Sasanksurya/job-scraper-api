b7824f63f7a45596ddcac96831555dce089f23be
Sasank Surya <shashanksurya24@gmail.com>
2026-05-02 15:34:55 +0530

    final fix jobs api

--- api/main.py
+++ api/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 from database.db import SessionLocal
 from database.models import Job
+from database.init_db import init_db   # ✅ REQUIRED
+
 app = FastAPI()
+
+@app.on_event("startup")
+def startup():
+    init_db()   # ✅ REQUIRED
+
 @app.get("/jobs")
 def get_jobs(page: int = 1, page_size: int = 5):
     session = SessionLocal()
 
     offset = (page - 1) * page_size
-
     jobs = session.query(Job).offset(offset).limit(page_size).all()
 
     result = []
@@ -19,6 +25,5 @@ def get_jobs(page: int = 1, page_size: int = 5):
             "location": job.location
         })
 
-    session.close()   # ✅ VERY IMPORTANT
-
+    session.close()
     return result
