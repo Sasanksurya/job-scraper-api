26d9d5e02c5a3c5178fed811e37ebd4fac6ca019
Sasank Surya <shashanksurya24@gmail.com>
2026-05-02 15:50:07 +0530

    add filtering to jobs api

--- api/main.py
+++ api/main.py
@@ -1,21 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from database.db import SessionLocal
 from database.models import Job
-from database.init_db import init_db   # ✅ added
+from database.init_db import init_db
 
 app = FastAPI()
 
 @app.on_event("startup")
 def startup():
-    init_db()   # ✅ this runs when app starts
+    init_db()
 
 
 @app.get("/jobs")
-def get_jobs(page: int = 1, page_size: int = 5):
+def get_jobs(
+    page: int = 1,
+    page_size: int = 5,
+    location: str = None,
+    title: str = None
+):
     session = SessionLocal()
 
+    query = session.query(Job)
+
+    # 🔥 filtering
+    if location:
+        query = query.filter(Job.location == location)
+
+    if title:
+        query = query.filter(Job.title.contains(title))
+
     offset = (page - 1) * page_size
-    jobs = session.query(Job).offset(offset).limit(page_size).all()
+
+    jobs = query.offset(offset).limit(page_size).all()
 
     result = []
     for job in jobs:
