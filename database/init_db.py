0afb84c89d9a07e50f06cc2945e1bbac7aff4ca2
Sasank Surya <shashanksurya24@gmail.com>
2026-05-02 15:23:20 +0530

    fix database init

--- database/init_db.py
+++ database/init_db.py
@@ -1,4 +1,17 @@
-from database.db import engine
-from database.models import Base
+from database.db import engine, SessionLocal
+from database.models import Base, Job
 
-Base.metadata.create_all(bind=engine)
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+    db = SessionLocal()
+
+    if db.query(Job).count() == 0:
+        db.add_all([
+            Job(title="Python Developer", company="ABC Company", location="India"),
+            Job(title="Data Analyst", company="XYZ Company", location="USA"),
+            Job(title="Backend Engineer", company="Google", location="USA")
+        ])
+        db.commit()
+
+    db.close()
