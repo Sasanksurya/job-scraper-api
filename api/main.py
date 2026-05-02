5395dbd76d8ec5b81730e3d5392217f72a911761
Sasank Surya <shashanksurya24@gmail.com>
2026-05-02 15:31:38 +0530

    fix jobs api

--- api/main.py
+++ api/main.py
@@ -1,3 +1,7 @@
+from fastapi import FastAPI
+from database.db import SessionLocal
+from database.models import Job
+app = FastAPI()
 @app.get("/jobs")
 def get_jobs(page: int = 1, page_size: int = 5):
     session = SessionLocal()
