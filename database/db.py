5bc7b34f29bd49121a12e2920136fc8ebe620479
Sasank Surya <shashanksurya24@gmail.com>
2026-05-02 15:04:27 +0530

    switch to sqlite

--- database/db.py
+++ database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:surya@localhost/job_db"
+DATABASE_URL = "sqlite:///./jobs.db"
 
 engine = create_engine(DATABASE_URL)
 
